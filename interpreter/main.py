21463264916ff8c0e83684d43d316119b99c9bbf
Alexander Polynomdivision <digitalmail555@googlemail.com>
2019-02-07 18:50:45 +0100

    feat: Add (stubbed) returns and fix the callstack

--- interpreter/main.py
+++ interpreter/main.py
@@ -1,10 +1,14 @@
 import sys
 
+global DEBUG_MODE
+DEBUG_MODE = False
+
 def debug(msg):
     """
     Print a debug message.
     """
-    print("[DEBUG] {}".format(msg))
+    if DEBUG_MODE:
+        print("[DEBUG] {}".format(msg))
 
 def warn(msg):
     """
@@ -39,17 +43,16 @@ class Function(object):
         self.instructions = instructions
 
 class Module(object):
-    def __init__(self, name, functions, main_function):
+    def __init__(self, name, functions, main_function, variables):
         stub("Module")
-        
+
         self.name = name
         self.functions = functions
         self.main_function = main_function
+        self.variables = variables
 
 class CallstackEntry:
     def __init__(self, function, instruction_index):
-        stub("CallstackEntry")
-
         self.function = function
         self.instruction_index = instruction_index
 
@@ -58,13 +61,24 @@ class Interpreter:
     Global attributes:
     @root_module (Object): The module of the module we're trying to load
     @function_stack (Array): The callstack of the interpreter
-    @variables (Dictionary): A Map of all known global variables?
     """
 
     def __init__(self, root_module):
         self.root_module = root_module
+        self.modules = []
         self.callstack = []
-        self.variables = {}
+
+        # Set up hooks
+        self.function_call_hooks = {
+            "print_callstack": self.print_callstack,
+            "print_variables": self.print_variables
+        }
+
+    def callstack_peek(self):
+        """
+        Returns the top of the callstack without removing it.
+        """
+        return self.callstack[-1]
 
     def get_main_function(self):
         """
@@ -84,8 +98,7 @@ class Interpreter:
         main_function = self.get_main_function()
 
         if not main_function is None:
-            self.callstack.append(CallstackEntry(None, -1))
-            self.execute_function(main_function, None, -1)
+            self.execute_function(main_function, None, 0)
         else:
             error("No entry point found")
 
@@ -93,27 +106,28 @@ class Interpreter:
         """
         Just print the state of self.callstack
         """
-        debug("Printing callstack")
+        print("---- Callstack ---- ")
         entries = list(range(0, len(self.callstack), 1))
         entries.reverse()
         for i in entries:
             if self.callstack[i].function is None:
-                debug("Root caller")
+                print("Root caller")
                 continue
 
             line = "{} at instruction {}".format(
                 self.callstack[i].function.name,
                 self.callstack[i].instruction_index)
 
-            debug(line)
+            print(line)
+        print("---- End ----")
 
     def print_variables(self):
         """
         Dump the variables
         """
-        debug("Dumping variables")
-        for key, val in self.variables.items():
-            debug("{}: {}".format(key, val))
+        #debug("Dumping variables")
+        for key, val in self.root_module.variables.items():
+            print("{}: {}".format(key, val))
 
     def get_function(self, name):
         """
@@ -132,7 +146,7 @@ class Interpreter:
 
         return None
 
-    def execute_instruction(self, instruction):
+    def execute_instruction(self, instruction, function, instruction_index):
         """
         Try and execute a single instruction
 
@@ -140,6 +154,9 @@ class Interpreter:
           @instruction: An array following the specification for an instruction:
               (<OPCODE>, <ARGUMENTS>). If <Arguments> contains another
               array, then it will be evaluated when the argument is processed.
+          @function: The function to which this instruction belongs.
+          @instruction_index: The index of the instruction list from which we
+              jumped here.
 
         Returns:
           In case that the opcode specifies a return value, it will be returned
@@ -149,55 +166,90 @@ class Interpreter:
         if opcode == "PRINT":
             # TODO: Evaluate the next argument if neccessary
             if not type(instruction[1]).__name__ == "str":
-                value = self.execute_instruction(instruction[1])
+                value = self.execute_instruction(instruction[1],
+                                                 function,
+                                                 instruction_index)
             else:
                 value = instruction[1]
 
             print(value)
         elif opcode == "SET":
             # TODO: Evaluate the next argument if neccessary
-            self.variables[instruction[1]] = instruction[2]
-
-            # TODO: Remove
-            # self.print_variables()
+            if not type(instruction[2]).__name__ == "str":
+                debug("Evaluating argument of opcode SET")
+                value = self.execute_instruction(instruction[2],
+                                                 function,
+                                                 instruction_index)
+            else:
+                value = instruction[2]
+            self.root_module.variables[instruction[1]] = value
         elif opcode == "GET":
             # TODO: Do we know the variable
-            return self.variables[instruction[1]]
+            if not instruction[1] in self.root_module.variables.keys():
+                error("Variable {} not defined".format(instruction[1]))
+                # TODO: Handle this case more gracefully?
+                sys.exit(1)
+
+            return self.root_module.variables[instruction[1]]
         elif opcode == "CALL":
             call = instruction[1]
 
-            self.execute_function(self.get_function(call),
-                                  None,
-                                  -1)
+            if call in self.function_call_hooks.keys():
+                self.function_call_hooks[call]()
+                return None
+
+            debug("Calling function {}".format(call))
+            return self.execute_function(self.get_function(call),
+                                         function,
+                                         instruction_index)
+        elif opcode == "RETURN":
+            if not type(instruction[1]).__name__ == "str":
+                return self.execute_instruction(instruction[1],
+                                                function,
+                                                instruction_index)
+            return instruction[1]
         else:
             not_implemented("Language function {}".format(instruction[0]))
 
         return None
 
-    def execute_function(self, function, caller, caller_instruction_index):
+    def execute_function(self,
+                         function,
+                         caller_function,
+                         caller_instruction_index):
         """
         Try to execute a given function
 
         Args:
-          function: The function to execute
-        """
-        not_implemented("Interpreter.execute_function")
+          @function: The function to execute
+          @caller: The calling function
+          @caller_instruction_index: Where was the caller when the call was
+              issued
 
-        # TODO: Add the caller and the instruction index to the callstack
+        Returns:
+          Returns the value specified by the function by using a return
+          statement. None otherwise.
+        """
+        # Add the caller to the callstack
+        self.callstack.append(CallstackEntry(caller_function,
+                                             caller_instruction_index))
 
         # Work on executing the function
+        instruction_index = 0
         for instruction in function.instructions:
-            self.execute_instruction(instruction)
+            debug(instruction)
+            ret = self.execute_instruction(instruction,
+                                           function,
+                                           instruction_index)
+            if instruction[0] == "RETURN":
+                debug("Found a return instruction!")
+                debug("Returning \"{}\"".format(ret))
+                self.callstack.pop(-1)
+                return ret
+            instruction_index += 1
 
-        # TODO: Remove ourselves from the callstack, if it was not already
-        #       done by an early return
-
-        # self.print_callstack()
-        # Jump back if the caller is not null
-        # return_to = self.callstack.pop()
-        # if not return_to == None:
-        #     self.execute_function()
-
+        self.callstack.pop(-1)
+        return None
 
 def print_usage():
     """
@@ -225,16 +277,20 @@ def main():
         print("Anything other than interpreter mode is unsupported")
         print()
         print_usage()
-        
+
         sys.exit(1)
 
+    # TODO: Move this inside the Interpreter?
+    global DEBUG_MODE
+    DEBUG_MODE = "-d" in sys.argv or "--debug" in sys.argv
+ 
     # Tests
-    test_file = sys.argv[2]
+    test_file = sys.argv[-1]
     if test_file == "1":
         test_module = Module("test_module", [
             Function("print", []),
             Function("main", [])
-        ], None)
+        ], None, {})
     elif test_file == "2":
         test_module = Module("test_module", [
             Function("print", []),
@@ -251,7 +307,62 @@ def main():
             ("PRINT", ("GET", "A")),
             ("CALL", "not_main"),
             ("PRINT", "We returned")
-        ]))
+        ]), {})
+    elif test_file == "inv_var":
+        test_module = Module("test_module", [
+            Function("print", []),
+            Function("not_main", [
+                ("PRINT", "Called not_main")
+            ])
+        ], Function("actual_main", [
+            ("PRINT", "HALLO"),
+            ("PRINT", "JONAS"),
+            ("SET", "A", "1"),
+            ("PRINT", ("GET", "B"))
+        ]), {})
+    elif test_file == "fun_hook":
+        test_module = Module("test_module", [
+            Function("print", []),
+            Function("not_main", [
+                ("PRINT", "Called not_main")
+            ])
+        ], Function("actual_main", [
+            ("CALL", "print_callstack")
+        ]), {})
+    elif test_file == "callstack_test":
+        test_module = Module("test_module", [
+            Function("f1", [
+                ("CALL", "print_callstack"),
+                ("CALL", "f2")
+            ]),
+            Function("f2", [
+                ("CALL", "print_callstack"),
+                ("CALL", "f3")
+            ]),
+            Function("f3", [
+                ("CALL", "print_callstack"),
+                ("PRINT", "Done")
+            ]),
+        ], Function("actual_main", [
+            ("PRINT", "Hallo"),
+            ("CALL", "f1"),
+            ("CALL", "print_callstack")
+        ]), {})
+    elif test_file == "fun_test":
+        test_module = Module("test_module", [
+            Function("return_stuff", [
+                ("PRINT", "I am inside return_stuff"),
+                ("RETURN", "hallo"),
+            ])
+        ], Function("actual_main", [
+            ("SET", "A", ("CALL", "return_stuff")),
+            ("PRINT", ("GET", "A")),
+            ("PRINT", "DONE")
+        ]), {})
+    else:
+        print("Invalid test specified")
+        sys.exit(1)
+
     intr = Interpreter(test_module)
     intr.run_module()
 
